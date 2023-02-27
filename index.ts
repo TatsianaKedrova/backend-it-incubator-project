494b9615f5f8599dbe97f0679c3be050f987ac9a
TatsianaKedrova <Baletrot@gmail.com>
2023-02-27 11:17:11 +0200

    removed node_modules and .env from the project

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+require("dotenv").config();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/users', (req, res) => {
-  res.send('Hello Users!')
-})
+app.get("/users", (req, res) => {
+  res.send("Hello Users!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
